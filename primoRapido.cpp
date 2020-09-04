bade3e7d1e72c3d87568d347034c252473e9e830
artur henrique <henrique-artur@hotmail.com>
2020-09-04 01:32:10 -0300

    Create primoRapido.cpp
    Questão1221- Primo Rápido

---
+++ primoRapido.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int numPrimo(long long int n);
+    int casos;
+    long long int num;
+    cin >> casos;
+    while (casos--){
+        cin >> num;
+        if(1 == numPrimo(num))
+            cout << "Prime\n";
+        else
+            cout << "Not Prime\n";
+            
+    }
+    return 0;
+}
+
+int numPrimo(long long int n){
+    int i, root;
+    if (n<2)
+        return 0;
+    if (n==2)
+        return 1;
+    if (n%2==0)
+        return 0;
+    root = sqrt(n);
+    for (i = 3; i <= root; i+=2)
+        if (n%i == 0)
+            return 0;
+    return 1;            
+}
