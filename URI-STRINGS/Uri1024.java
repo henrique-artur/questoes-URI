3e0fb6993fefde7181be6d5e2feb609668d6a0bf
henrique-artur <henrique-artur@hotmail.com>
2021-01-26 08:32:49 -0300

    substituicao de arquivos

---
+++ URI-STRINGS/Uri1024.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Uri1024 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int i = leitor.nextInt();
+        clearBuffer(leitor);
+
+        while (i > 0){
+
+            String entrada = leitor.nextLine();
+
+            int[] listaUnicode = extrairUnicode(entrada);
+
+            listaUnicode = primeiroPasso(listaUnicode);
+            listaUnicode = reverse(listaUnicode);
+            listaUnicode = terceiroPasso(listaUnicode);
+
+            System.out.println(decode(listaUnicode));
+            i--;
+        }
+
+    }
+    private static void clearBuffer(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
+    }
+    private static String decode(int[] lista){
+        String strFinal = "";
+        for (int i = 0; i < lista.length; i++){
+            strFinal += (char) lista[i];
+        }
+        return strFinal;
+    }
+
+    private static int[] primeiroPasso(int[] lista){
+        for(int i = 0; i < lista.length; i++){
+            if(lista[i] >= 65 && lista[i] <= 90 ||
+                    lista[i] >= 97 && lista[i] <= 122){
+                lista[i] += 3;
+            }
+        }
+        return lista;
+    }
+    private static int[] reverse(int[] lista){
+
+        int tamanho = lista.length;
+        int[] arrayAux = new int[tamanho];
+
+        for (int i = 0; i < arrayAux.length; i++) {
+            tamanho--;
+            arrayAux[i] = lista[tamanho];
+        }
+        return arrayAux;
+    }
+    private static int[] extrairUnicode(String texto){
+        int[] lista = new int[texto.length()];
+        for (int i = 0; i < texto.length(); i++){
+            lista[i] = texto.codePointAt(i);
+        }
+        return lista;
+    }
+    private static int[] terceiroPasso(int[] lista){
+
+        for (int i = lista.length/2; i < lista.length; i++){
+            if(lista[i] >= 32 && lista[i] <= 176){
+                lista[i] -= 1;
+            }
+        }
+        return lista;
+    }
+}
